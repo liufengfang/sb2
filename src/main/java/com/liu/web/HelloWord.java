b1c3be4c2a284b838677cfe5f65c2a22624fb270
liufengfang <liufengfang2017@icloud.com>
2018-11-24 11:26:49 +0800

    添加会html的支持

--- src/main/java/com/liu/web/HelloWord.java
+++ src/main/java/com/liu/web/HelloWord.java
@@ -8,13 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWord {
 
-    @RequestMapping("/")
-    @ResponseBody
+    @RequestMapping("/helloWorld")
     String home() {
-        return "Hello World! java -jar target/sb2-0.0.1-SNAPSHOT.jar ";
+        return "easy1";
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(HelloWord.class, args);
-    }
+//    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(HelloWord.class, args);
+//    }
 }
