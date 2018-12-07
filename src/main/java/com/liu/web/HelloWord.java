8eae42ff344d1d7fe8ca1b7ce71071f75e0cdcd9
liufengfang <liufengfang2017@icloud.com>
2018-12-08 00:00:01 +0800

    测试RequestMapping规则，不直接声明RequstMethod的场景

--- src/main/java/com/liu/web/HelloWord.java
+++ src/main/java/com/liu/web/HelloWord.java
@@ -1,9 +1,12 @@
 package com.liu.web;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloWord {
@@ -13,7 +16,35 @@ public class HelloWord {
         return "easy1";
     }
 
-//    public static void main(String[] args) throws Exception {
-//        SpringApplication.run(HelloWord.class, args);
-//    }
+    @RequestMapping(value = "/testGet",method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, String> requestMappingWithGET() {
+        //仅GET请求会进入本方法
+        Map<String, String> map = new HashMap<>();
+        map.put("method", "requestMappingWithGET");
+
+        return map;
+    }
+    @RequestMapping(value = "/testGet")
+    @ResponseBody
+    public Map<String, String> requestMappingWithNothing() {
+        //类似兜底的方法，
+        // 除明确定义了RequestMethod的请求外，其他请求都会进入本方法
+        Map<String, String> map = new HashMap<>();
+        map.put("method", "requestMappingWithNothing");
+
+        return map;
+    }
+
+    @RequestMapping(value = "/testGet", method = RequestMethod.PUT)
+    @ResponseBody
+    public Map<String, String> requestMappingWithPUT(){
+        //仅PUT请求会进入本方法
+        Map<String, String> map = new HashMap<>();
+        map.put("method", "requestMappingWithPUT");
+
+        return map;
+    }
+
+
 }
