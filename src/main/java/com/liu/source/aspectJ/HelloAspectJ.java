65036d9f3c04ffd27587e4c098016638a248177c
liufengfang <liufengfang2017@icloud.com>
2019-03-28 23:35:23 +0800

    add demo

--- src/main/java/com/liu/source/aspectJ/HelloAspectJ.java
+++ src/main/java/com/liu/source/aspectJ/HelloAspectJ.java
@@ -1,11 +1,6 @@
 package com.liu.source.aspectJ;
 
-import com.liu.source.chap2.MyTestBean;
-import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
 
 /**
  * Created by liufengfang on 2019/3/16.
@@ -17,10 +12,8 @@ public class HelloAspectJ {
 
 	@Test
 	public void mmmm() {
-//		HelloAspectJ ha = new HelloAspectJ();
-//		ha.sayHelloAspect();
-		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("spring-source-test.xml"));
-		MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
-		Assert.assertEquals("testStr",myTestBean.getTestStr());
+		HelloAspectJ ha = new HelloAspectJ();
+		//todo
+		ha.sayHelloAspect();
 	}
 }
---
+++ src/main/java/com/liu/source/chap2/SpringBeansDemo.java
@@ -0,0 +1,19 @@
+package com.liu.source.chap2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Created by liufengfang on 2019/3/28.
+ */
+public class SpringBeansDemo {
+    @Test
+    public void mmmm() {
+        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("spring-source-test.xml"));
+        MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
+        Assert.assertEquals("testStr",myTestBean.getTestStr());
+    }
+}
