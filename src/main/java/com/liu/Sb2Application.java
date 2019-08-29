b1444c0202aab97717113ff27784068685feddb8
刘丰方 <liufengfang2017@icloud.com>
2019-08-29 16:54:22 +0800

    更改打包方式为war，并使其可以被部署在外部tomcat中

--- src/main/java/com/liu/Sb2Application.java
+++ src/main/java/com/liu/Sb2Application.java
@@ -2,11 +2,18 @@ package com.liu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Sb2Application {
+public class Sb2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sb2Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
