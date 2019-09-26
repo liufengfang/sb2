9b907a2f858507397fd65862deb2d82f6c5b7193
刘丰方 <liufengfang2017@icloud.com>
2019-09-26 17:21:35 +0800

    register my interceptors to apply to incoming requests

---
+++ src/main/java/com/liu/InterceporConfig.java
@@ -0,0 +1,18 @@
+package com.liu;
+
+import com.liu.source.AOP.MyInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by liufengfang on 2019/9/26.
+ */
+@Configuration
+public class InterceporConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/com/liu/source/AOP/MyInterceptor.java
@@ -0,0 +1,33 @@
+package com.liu.source.AOP;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by liufengfang on 2019/9/26.
+ */
+public class MyInterceptor implements HandlerInterceptor {
+    protected final Log logger = LogFactory.getLog(this.getClass());
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("preHandle==========");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("postHandle==========");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        logger.info("afterCompletion==========");
+    }
+}
