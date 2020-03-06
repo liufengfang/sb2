6f86e8d281594834aa01b2956defcc018372d162
刘丰方 <liufengfang2017@icloud.com>
2020-03-06 16:28:00 +0800

    添加profiles

--- src/main/java/com/liu/InterceporConfig.java
+++ src/main/java/com/liu/InterceporConfig.java
@@ -2,12 +2,14 @@ package com.liu;
 
 import com.liu.source.AOP.MyInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created by liufengfang on 2019/9/26.
  */
+@Profile("dev")
 @Configuration
 public class InterceporConfig implements WebMvcConfigurer {
 
