75d1ac14433e1cd0470cb0f221fbe23b4e45292e
刘丰方 <liufengfang2017@icloud.com>
2020-03-07 15:48:42 +0800

    测试按照类型获取spring bean产生歧义的场景

--- src/main/java/com/liu/InterceptorConfig.java
+++ src/main/java/com/liu/InterceptorConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Profile("dev")
 @Configuration
-public class InterceporConfig implements WebMvcConfigurer {
+public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- src/main/java/com/liu/source/chap2/SpringBeansDemo.java
+++ src/main/java/com/liu/source/chap2/SpringBeansDemo.java
@@ -1,10 +1,12 @@
 package com.liu.source.chap2;
 
-import org.junit.Assert;
+import com.liu.annotation.Person;
+import com.liu.annotation.conditional.ConditionConfig;
 import org.junit.Test;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * Created by liufengfang on 2019/3/28.
@@ -12,8 +14,22 @@ import org.springframework.core.io.ClassPathResource;
 public class SpringBeansDemo {
     @Test
     public void mmmm() {
-        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("spring-source-test.xml"));
-        MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
-        Assert.assertEquals("haha,I'm myTestBean",myTestBean.getTestStr());
+//        BeanFactory bf = new XmlBeanFactory(new ClassPathResource("spring-source-test.xml"));
+//        MyTestBean myTestBean = (MyTestBean) bf.getBean("myTestBean");
+//        Assert.assertEquals("haha,I'm myTestBean",myTestBean.getTestStr());
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ConditionConfig.class);
+        System.err.println(Arrays.toString(ac.getBeanDefinitionNames()));
+
+        /*
+        org.springframework.beans.factory.NoUniqueBeanDefinitionException:
+        No qualifying bean of type 'com.liu.annotation.Person' available:
+        expected single matching bean but found 2: person,person2
+        */
+//        Person p = ac.getBean(Person.class);
+
+
+        Person p = ac.getBean("person", Person.class);
+        Person p2 = ac.getBean("person2", Person.class);
     }
 }
