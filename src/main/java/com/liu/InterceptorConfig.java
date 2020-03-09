a69e160185a1d7c813e3bb98e65174b4714e68a4
刘丰方 <liufengfang2017@icloud.com>
2020-03-09 16:04:21 +0800

    注入外部的值，和探索spring 的 Environment

--- src/main/java/com/liu/InterceptorConfig.java
+++ src/main/java/com/liu/InterceptorConfig.java
@@ -1,8 +1,13 @@
 package com.liu;
 
+import com.liu.annotation.Person;
 import com.liu.source.AOP.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,10 +16,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Profile("dev")
 @Configuration
+@PropertySource("classpath:application.yml")
 public class InterceptorConfig implements WebMvcConfigurer {
+    @Autowired
+    private Environment env;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    public Person person3() {
+        Person p = new Person(
+                env.getProperty("spring.application.name"), "kkk");
+        return p;
+
+
     }
 }
